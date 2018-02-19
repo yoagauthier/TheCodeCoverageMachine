9d9aa6cf146f4955e1c98e82915a7acf4998740f
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-02-19 15:25:01 +0100

    Adding function to get the paths corresponding to variable values in the cover graph

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from model.criteria import TA
+# from model.criteria import TA
 from model.cover_graph import CoverGraph
+from model.astree import ASTree
 
 if __name__ == "__main__":
 
     # load the programm into an ASTree
     # get the initial values of the variables
 
-    # convert the ASTree to a CoverGraph
-    initial_values = [0, 1]
-    cover_graph = CoverGraph()
+    tree = ASTree('Examples/exemple.txt')
+    CG = tree.to_cover_graph()
+    CG.get_all_paths([{'X': 12}])
 
-    criterias = [TA]
-    for criteria in criterias:
-        for value in initial_values:
-            print(criteria, " : ", criteria.check(cover_graph, value))
+    # criterias = [TA]
+    # for criteria in criterias:
+    #     for value in initial_values:
+    #         print(criteria, " : ", criteria.check(cover_graph, value))
--- model/cover_graph.py
+++ model/cover_graph.py
@@ -5,9 +5,6 @@ class CoverGraph(object):
         self.vertices = vertices
         self.edges = edges
 
-    def to_execution_path_tree(self):
-        pass
-
     def renamed_edges(self, old_vertex, new_vertex):
         new_edges = []
         for edge in self.edges:
@@ -24,46 +21,41 @@ class CoverGraph(object):
             new_edges.append(edge)
         return new_edges
 
-    # def to_ep_tree(self):
-    #     # need to do a BFS
-    #     self.L = []  # list of verteces
-    #     T = []
-    #     self.visit_node(self.root_node)
-    #     while self.L:
-    #         root, child = self.L.pop(0)
-    #         if not child.visited:
-    #             T.append((root, child))
-    #             self.visit_node(child)
-    #
-    #     return T  # execution_path_tree
-    #
-    # def get_next_node(self, current_node, variable):
-    #     pass
-
-    def get_path(self, initial_variable):
-        # DRAFT
-        # doing a BFS, need to return a list of node and edges
-        L = []
-        L.append(self.root_vertex)
-        var = initial_variable
-        while L:
-            child = self.get_next_node(L.pop(), var)
-            var = child.execute_step(var)
-            L.append(child)
-
+    def get_path(self, test_set):
         # on part de la root node
         # on prend pour chaque node la successeur, en vérifiant la condition
         # on s'arrête à la node finale
         # return  path, qui est une liste d'edge et de nodes
+        L = []
+        values = test_set
+        vertex = self.root_vertex
+        L.append(self.root_vertex)
+        while vertex.label != '_':
+            out_edges = [edge for edge in self.edges if edge.root_vertex == vertex]
+            # choose which edge to take
+            for edge in out_edges:
+                if edge.eval(values):  # ie eval was possible and gave a valid path
+                    out_edge = edge
+                    values = edge.eval(values)
+                    break
+            if not out_edge:
+                raise "Programm non executable"
+            vertex = out_edge.child_vertex
+            L.append(vertex)
+        return L  # list of vertices that we've been through
+
+    def get_all_paths(self, test_sets):
+        # test set should like [{var1: val1, var2: val2, ...}, ...], each dict
+        # is a set of variables
+        paths = []
+        for test_set in test_sets:
+            paths += self.get_path(test_set)
+        print("PATHS", paths)
+        return paths
 
     def get_all_nodes(self):
         """Returns all the nodes of the cover graph"""
         raise NotImplementedError
-
-    # def visit_node(self, node):
-    #     node.visited = True
-    #     for vertex in (self.vertices.root_node, self.vertices.child_node):
-    #         self.L.append(vertex)
 
     def __str__(self):
         return '\n'.join([str(edge) for edge in self.edges])
@@ -95,6 +87,13 @@ class Edge(object):
         self.child_vertex = child_vertex
         self.condition = condition  # boolean node
         self.operation = operation  # assignement or skip node
+
+    def eval(self, values):
+        """Returns a value if the eval was possible, None otherwise"""
+        if self.condition.eval(values):
+            return self.operation.eval(values)
+        else:
+            return None
 
     def __str__(self):
         return '{} --({}/{})--> {}'.format(
